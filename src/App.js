9686276b35e1db99c865826dcebb79b965a1bdee
eddanieles <eddanieles@gmail.com>
2020-12-28 06:06:33 -0500

    materialUI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from './routes/Home'
 import SignUp from './routes/SignUp'
 import Login from './routes/Login'
+import Ratings from './routes/Ratings'
 import { AuthProvider } from './Auth'
 import PrivateRoute from './PrivateRoute'
 
@@ -15,6 +16,7 @@ const App = () => {
           <PrivateRoute exact path="/" component={Home} />
           <Route exact path="/signup" component={SignUp} />
           <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/ratings" component={Ratings} />
         </div>
       </Router>
     </AuthProvider>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import app from '../base'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return ( 
     <div>
+        <Link to="ratings">Ratings</Link>
         <h1>Home</h1> 
         <button onClick={() => app.auth().signOut() }>Sign Out</button>
     </div>
